0bfcbf6699850fc8f3dacbe9d239f3b4d44e309a
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-01-15 20:28:02 -0300

    anotações

--- main.js
+++ main.js
@@ -11,9 +11,14 @@
 // })
 
 // Utilizando jquery
+// .quando o documento estiver carregado, aplicar mask ao campo de id #cep
 $(document).ready(function() {
     $('#cep').mask('00000-000');
-
+// .qndo o item de id #btn-buscar-cep receber um clique, rodar a função seguinte:
+        // - armazanar os valores recebidos no input #cep na const cep.
+        // - criar um array de "endpoints/urls" para cada item armazenado em cep
+        // - criar uma const botão que representa "this", ou seja, #btn-buscar-cep
+        //  - encontrar botao no html e, se achar a tag i, add classe d-none. se achar a tag span, remover class d-none
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
@@ -21,6 +26,7 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
+// Config resposta:
         $.ajax(endpoint).done(function(resposta) {
             const logradouro = resposta.logradouro;
             const bairro = resposta.bairro;
@@ -28,7 +34,8 @@ $(document).ready(function() {
             const estado = resposta.uf;
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco)
-
+            
+// Forçando um delay para que a animação do ícone seja visível:
             setTimeout(function() {
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
