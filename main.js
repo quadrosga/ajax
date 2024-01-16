002e60bc58c56d1203da45502902d76badbb22e6
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-01-16 18:11:04 -0300

    fetch API, trat. exceção com catch e finally

--- main.js
+++ main.js
@@ -26,20 +26,49 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-// Config resposta:
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco)
+// Config resposta, utilizando AJAX:
+//         $.ajax(endpoint).done(function(resposta) {
+//             const logradouro = resposta.logradouro;
+//             const bairro = resposta.bairro;
+//             const cidade = resposta.localidade;
+//             const estado = resposta.uf;
+//             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+//             $('#endereco').val(endereco);
             
-// Forçando um delay para que a animação do ícone seja visível:
+// // Forçando um delay para que a animação do ícone seja visível:
+//             setTimeout(function() {
+//                 $(botao).find('i').removeClass('d-none');
+//                 $(botao).find('span').addClass('d-none');
+//             },1000);
+//         })
+
+// Config resposta, utilizando o fetch API:
+        fetch(endpoint)
+        .then(function(resposta) {
+            return resposta.json();
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+        })
+        // o then acima já funciona como try.
+        // é necessário agora config. o catch para o caso de erro
+        // a sintaxe do catch é diferente, pois o fetch trabalha com
+        // promises (an object that'll produce a single value some time in the future)
+        .catch(function(erro) {
+            alert("Ocorreou um erro ao buscar o endereço. Tente novamente mais tarde.");
+        })
+        // agora, config o finally para que a remoção/adição da classe da animação
+        // do botão ocorra ao fim, independente de erro ou sucesso.
+        .finally(function() {
             setTimeout(function() {
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
-            },1000);
+            }, 1000);
         })
     })
 })
